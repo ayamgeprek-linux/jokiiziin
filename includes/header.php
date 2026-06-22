3c774138ad47e8845179949dd66997dac122267f
ayamgeprek-linux <torenoren300@gmail.com>
2026-06-23 02:36:49 +0700

    perbaikan css

--- includes/header.php
+++ includes/header.php
@@ -32,7 +32,7 @@ $recentNotif = $notifManager->getRecentNotifikasi(5);
     <title>doret.id — Manajemen Cuti Karyawan</title>
     
     <!-- CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.min.css">
     
     <style>
